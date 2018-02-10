5f0314a5c3eeb7dcb36d6b50941cdee23177557a
xTEddie <edwardtranv1@gmail.com>
2018-02-10 17:59:48 -0500

    Add bookmarks.sample.js

---
+++ js/bookmarks.sample.js
@@ -0,0 +1,12 @@
+let bookmarks = [
+  {
+    name: 'Bookken',
+    url: 'https://github.com/xTEddie/Bookken',
+    icon: 'fab fa-github fa-2x'
+  },
+  {
+    name: 'chromecast-backgrounds',
+    url: 'https://github.com/dconnolly/chromecast-backgrounds',
+    icon: 'fab fa-github fa-2x'   
+  }
+]
